27959c6415c6daa08289f3d7e666bb5b38e90a21
kzkstk <kazuki-10.6.10@ezweb.ne.jp>
2025-07-29 18:29:23 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,14 +1,14 @@
-const num = 15;
+const num = 22;
 
 if (num % 3 ==0 && num % 5 ==0) {
-  console.log('3の倍数かつ5の倍数です');
+  console.log('3と5の倍数です');
 }
 
 else if (num % 5 == 0) {
   console.log('5の倍数です');
 }
 
-else if (num % 3 ==0) {
+else if (num % 3 == 0) {
   console.log('3の倍数です');
 }
 
